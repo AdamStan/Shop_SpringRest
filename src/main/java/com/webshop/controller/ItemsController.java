14be2941a7b95027115f05cad7db0b8167eab93c
AdamStan <adam15stan@gmail.com>
2019-08-04 17:26:35 +0200

    small changes for front, adding crossorigin annotation

--- src/main/java/com/webshop/controller/ItemsController.java
+++ src/main/java/com/webshop/controller/ItemsController.java
@@ -7,15 +7,16 @@ import com.webshop.model.items.Movie;
 import com.webshop.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.naming.OperationNotSupportedException;
 import java.util.List;
 
 @Controller
 @RequestMapping(path = "/items")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ItemsController {
     @Autowired
     public ItemRepository<Item> itemsRepository;
